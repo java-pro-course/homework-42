9e8a13e2b494c6507a4f6676b46becd9b63d5753
Artem <artemtarbeevv@gmail.com>
2023-03-12 11:43:52 +0500

    1, 2

---
+++ 1/demo/src/main/java/codemika/com/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package codemika.com.demo;
+
+import codemika.com.demo.annotation.SimpleHelloAnnotation;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
